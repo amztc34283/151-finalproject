a51c80461e3bbab9914e2e540e090440a742b341
amztc34283 <chunheichau@berkeley.edu>
2019-11-11 00:08:46 -0800

    added offset capability to ld_sel and testing

--- hardware/sim/ld_sel_testbench.v
+++ hardware/sim/ld_sel_testbench.v
@@ -8,41 +8,54 @@ module ld_sel_testbench();
 
     reg [2:0] sel = 0;
     reg [31:0] din = 0;
+    reg [1:0] offset = 0;
     wire [31:0] dout;
 
     ld_sel DUT(.sel(sel),
                .din(din),
-               .dout(dout));
+               .dout(dout),
+               .offset(offset));
+
+    /**
+    2.9.2
+    Unaligned Memory Accesses
+    In the official RISC-V specification, unaligned loads and stores are supported. However, in your
+    project, you can ignore instructions that request an unaligned access. Assume that the compiler
+    will never generate unaligned accesses.
+    **/
 
     initial begin
         //Load Byte
         sel = 0;
+        offset = 0;
         din = 32'hffffffff;
         #1;
-        if (dout != 32'h000000ff) begin
-            $display("Failed Load Byte Test Case");
+        if (dout != 32'hffffffff) begin
+            $display("Failed Load Byte with zero offset Test Case");
         end
         din = 32'habcdef12;
         #1;
         if (dout != 32'h00000012) begin
-            $display("Failed Load Byte Test Case");
+            $display("Failed Load Byte with zero offset Test Case");
         end
 
         //Load Halfword
         sel = 1;
+        offset = 0;
         din = 32'hffffffff;
         #1;
-        if (dout != 32'h0000ffff) begin
-            $display("Failed Load Halfword Test Case");
+        if (dout != 32'hffffffff) begin
+            $display("Failed Load Halfword with zero offset Test Case");
         end
         din = 32'habcdef12;
         #1;
-        if (dout != 32'h0000ef12) begin
-            $display("Failed Load Halfword Test Case");
+        if (dout != 32'hffffef12) begin
+            $display("Failed Load Halfword with zero offset Test Case");
         end
 
         //Load Word
         sel = 2;
+        offset = 0;
         din = 32'hffffffff;
         #1;
         if (dout != 32'hffffffff) begin
@@ -54,8 +67,107 @@ module ld_sel_testbench();
             $display("Failed Load Word Test Case");
         end
 
-        //Default case should be zero
+        //Load unsigned byte
         sel = 3;
+        offset = 0;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'h000000ff) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'h00000012) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+
+        //Load unsigned halfword
+        sel = 4;
+        offset = 0;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'h0000ffff) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'h0000ef12) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+
+
+        //With Offset
+        //Load Byte
+        sel = 0;
+        offset = 1;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'hffffffff) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'hffffffef) begin
+            $display("Failed Load Byte with zero offset Test Case");
+        end
+
+        //Load Halfword
+        sel = 1;
+        offset = 2;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'hffffffff) begin
+            $display("Failed Load Halfword with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'hffffabcd) begin
+            $display("Failed Load Halfword with zero offset Test Case");
+        end
+
+        //Load unsigned byte
+        sel = 3;
+        offset = 2;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'h000000ff) begin
+            $display("Failed Load unsigned Byte with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'h000000cd) begin
+            $display("Failed Load unsigned Byte with zero offset Test Case");
+        end
+
+        //Load unsigned halfword
+        sel = 4;
+        offset = 0;
+        din = 32'hffffffff;
+        #1;
+        if (dout != 32'h0000ffff) begin
+            $display("Failed Load unsigned Halfword with zero offset Test Case");
+        end
+        din = 32'habcdef12;
+        #1;
+        if (dout != 32'h0000ef12) begin
+            $display("Failed Load unsigned Halfword with zero offset Test Case");
+        end
+
+        //Default case should be zero (4 < sel)
+        sel = 5;
+        offset = 0;
+        #1;
+        if (dout != 32'h00000000) begin
+            $display("Failed Default Sel Test Case");
+        end
+        sel = 6;
+        offset = 1;
+        #1;
+        if (dout != 32'h00000000) begin
+            $display("Failed Default Sel Test Case");
+        end
+        sel = 7;
+        offset = 0;
         #1;
         if (dout != 32'h00000000) begin
             $display("Failed Default Sel Test Case");
--- hardware/src/riscv_core/ld_sel.v
+++ hardware/src/riscv_core/ld_sel.v
@@ -1,21 +1,35 @@
 `define LOAD_BYTE 0
 `define LOAD_HALFWORD 1
 `define LOAD_WORD 2
+`define LOAD_UNSIGNED_BYTE 3
+`define LOAD_UNSIGNED_HALFWORD 4
 
+/**
+sel - select the correct format of data to be loaded
+offset - DMEM address last two bits
+**/
 module ld_sel(
     input [2:0] sel,
     input [31:0] din,
+    input [1:0] offset,
     output reg [31:0] dout
 );
+    wire [31:0] offset_din;
+    wire [4:0] offset_extend;
+
+    //offset * 8
+    assign offset_extend = (offset << 3);
+
+    //shift right din by offset_extend
+    assign offset_din = din >> offset_extend;
 
     always @( * ) begin
         case (sel)
-            //Load Byte
-            `LOAD_BYTE  : dout = din[0 +: 8];
-            //Load Half
-            `LOAD_HALFWORD  : dout = din[0 +: 16];
-            //Load Word
-            `LOAD_WORD  : dout = din[0 +: 32];
+            `LOAD_BYTE : dout = $signed(offset_din[0 +: 8]);
+            `LOAD_HALFWORD : dout = $signed(offset_din[0 +: 16]);
+            `LOAD_WORD : dout = offset_din[0 +: 32];
+            `LOAD_UNSIGNED_BYTE : dout = offset_din[0 +: 8];
+            `LOAD_UNSIGNED_HALFWORD : dout = offset_din[0 +: 16];
             default : dout = 0;
         endcase
     end
