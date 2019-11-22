a528552faa90569825e8c11af1a68bb5e9d663f1
hsiehbrandon <hsiehbrandon@berkeley.edu>
2019-11-22 09:57:39 -0800

    cleanup files added by merge

--- hardware/DVEfiles/crash_11.17:15.03/synopsys_dve_prefs.tcl
+++
@@ -1,9 +0,0 @@
-# DVE version G-2012.09_Full64
-# Preferences written on Fri Sep 13 17:38:06 2019
-gui_set_var -name {read_pref_file} -value {true}
-gui_create_pref_key -category {Globals} -key {load_detail_for_funcov} -value_type {bool} -value {false}
-
-gui_create_pref_category -category {schem_obj_name}
-gui_create_pref_key -category {schem_obj_name} -key {show_cell_and_pin_with_symbol_name} -value_type {bool} -value {false}
-gui_create_pref_key -category {schem_pin_hc} -key {textExtFont} -value_type {string} -value {Helvetica,10,-1,5,50,0,0,0,0,0}
-gui_set_var -name {read_pref_file} -value {false}
