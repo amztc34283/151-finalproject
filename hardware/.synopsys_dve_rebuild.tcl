f7bd1c7aa6603132c64a84eb0352173edbc092d7
hsiehbrandon <hsiehbrandon@berkeley.edu>
2019-11-22 10:03:06 -0800

    more cleanup

--- hardware/.synopsys_dve_rebuild.tcl
+++
@@ -1,8 +0,0 @@
-# DVE Simulation Rebuild/Restart Options
-# Saved on Sun Nov 17 14:21:06 2019
-set SIMSETUP::REBUILDOPTION 1
-set SIMSETUP::REBUILDCMD {make sim/assembly_btype_testbench.vpd}
-set SIMSETUP::REBUILDDIR {/home/cc/eecs151/fa19/class/eecs151-abm/fa19_team28/hardware/}
-set SIMSETUP::RESTOREBP 1
-set SIMSETUP::RESTOREDUMP 1
-set SIMSETUP::RESTOREFORCE 1
